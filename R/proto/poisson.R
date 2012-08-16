255c40c4da03f7a72d228c34571e17b7186736de
Ryan J. Parker <ryan@basketballgeek.com>
2012-08-16 14:24:42 -0400

    Altered the way the continuous approximation is made

--- R/proto/poisson.R
+++ R/proto/poisson.R
@@ -1,13 +1,13 @@
 # fit correlated Poisson data with copula
 
-"corr_pois" <- function(y, V=0.5) {
+invlogit <- function(x) { 1/(1+exp(-x)) }
+
+"corr_pois" <- function(y, nV=1) {
 	# assumes constant lambda and same correlation for all
 	# Thus Sigma_ij = rho for all i != j and Sigma_ij = 1 otherwise
 
-	invlogit <- function(x) { 1/(1+exp(-x)) }
-
 	# to start let's just use nlm
-	ull <- function(theta, y, n, V, nV) {
+	ull <- function(theta, y, ystar, n, V, nV) {
 		lambda <- exp(theta[1])
 		#rho <- 2*invlogit(theta[2]) - 1
 		rho <- invlogit(theta[2])
@@ -24,25 +24,11 @@
 		fy <- dpois(y, lambda=lambda, log=TRUE)
 		efy <- exp(fy)
 
-if (0) { # way to compute for a single V
 		Fstar <- ppois(y-1, lambda=lambda) + (ystar-y) * exp(fy)
 		z <- qnorm(Fstar)
 
 		# -log lik
 		ull <- sum(log(diag(cholSigma))) + 0.5 * (t(z) %*% ( chol2inv(cholSigma) - diag(n) ) %*% z) - sum(fy)
-} else { # way to compute for multiple V
-
-		# -log lik
-		ull <- 0
-		Fstar <- ppois(y-1, lambda=lambda)
-		ull <- sum( unlist(
-			mclapply(V, function(v) {
-				z <- qnorm(Fstar + v * efy )
-				0.5 * (t(z) %*% ( invSigma - diag(n) ) %*% z)
-			}, mc.cores=4)
-		) )
-		ull <- ull/nV + sum(log(diag(cholSigma))) - sum(fy)
-}
 
 		if (is.na(ull)) { return(1e4) }
 
@@ -52,17 +38,26 @@ if (0) { # way to compute for a single V
 	# initial values
 	theta <- c(log(mean(y)), 0)
 
-	#ystar <- y + V
-	fit <- nlm(ull, theta, y=y, n=length(y), V=V, nV=length(V))
+	n <- length(y)
+
+	if (nV == 1) {
+		# use V = 0.5
+		V <- matrix(0.5, nrow=1, ncol=n)
+	} else {
+		V <- matrix(runif(nV * n), nrow=nV, ncol=n)
+	}
+	fit <- nlm(ull, theta, y=y, ystar=y+V[1,], n=length(y), V=V, nV=nV)
 }
 
 if (1) {
+
+require(MASS)
 
 # simulate data
 lambda <- 10
 rho <- 0.75
 
-n <- 250
+n <- 500
 
 Sigma <- matrix(rho, nrow=n, ncol=n)
 diag(Sigma) <- 1
@@ -76,9 +71,10 @@ iy <- rpois(n, lambda=lambda)
 #pdf("corr.pdf");hist(y,breaks=10);graphics.off()
 #pdf("ind.pdf");hist(iy,breaks=10);graphics.off()
 
+fit <- corr_pois(y, nV=1)
+print(fit)
 
-#fit <- corr_pois(y, V=0.5)
-# TODO: change V!! it needs to be for each y; maybe call it U?
-fit <- corr_pois(y, V=runif(10000))
+cat("Estimated lambda:",exp(fit$estimate[1]),"\n")
+cat("Estimated rho:",invlogit(fit$estimate[2]),"\n")
 
 }
