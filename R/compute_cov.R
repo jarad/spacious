2c8b107d19480801f4c26df27436b4aff19e1c24
Ryan J. Parker <ryan@basketballgeek.com>
2012-11-12 09:55:27 -0500

    Added convenience matern_rho function

--- R/compute_cov.R
+++ R/compute_cov.R
@@ -6,12 +6,21 @@
 	if (type == "exp") {
 		theta[1] * diag(nrow(D)) + theta[2] * exp(-theta[3] * D)
 	} else if (type == "matern") {
-		mid <- 2*D*theta[3]*sqrt(theta[4])
-		rho <- mid^theta[4] * besselK(mid, theta[4])/(2^(theta[4]-1) * gamma(theta[4]))
-		rho[is.na(rho)] <- 1
+#		mid <- 2*D*theta[3]*sqrt(theta[4])
+#		rho <- mid^theta[4] * besselK(mid, theta[4])/(2^(theta[4]-1) * gamma(theta[4]))
+#		rho[is.na(rho)] <- 1
 		#theta[1] * diag(nrow(D)) + theta[2] * mid^theta[4] * besselK(mid, theta[4])/(2^(theta[4]-1) * gamma(theta[4]))
-		theta[1] * diag(nrow(D)) + theta[2] * rho
+		#theta[1] * diag(nrow(D)) + theta[2] * rho
+		theta[1] * diag(nrow(D)) + theta[2] * matern_rho(theta, D)
 	} else {
 		stop(paste("Unknown covariance type:",type))
 	}
+}
+
+"matern_rho" <- function(theta, D) {
+	mid <- 2*D*theta[3]*sqrt(theta[4])
+	rho <- mid^theta[4] * besselK(mid, theta[4])/(2^(theta[4]-1) * gamma(theta[4]))
+	rho[is.na(rho)] <- 1
+
+	rho
 }
