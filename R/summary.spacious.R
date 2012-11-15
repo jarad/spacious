6ab3cc21bb14e4d4ed3065b57441b33c4d1517ad
Ryan J. Parker <ryan@basketballgeek.com>
2012-11-15 02:06:50 -0500

    Add p-value computations to summary

--- R/summary.spacious.R
+++ R/summary.spacious.R
@@ -2,8 +2,19 @@
 "summary.spacious" <- function(object, ...) {
 # TODO: figure out how to make nice tables of results
 
-	df.beta <- data.frame(beta=object$beta, se=object$se.beta)
+	p.beta <- sapply(object$beta/object$se.beta,
+		function(z) {
+			2*( 1-pnorm(abs(z)) )
+		})
+
+	df.beta <- data.frame(beta=object$beta, se=object$se.beta, p=p.beta)
 	df.theta <- data.frame(theta=object$theta, se=object$se.theta)
+
+	colnames(df.beta) <- c("Estimate","Std Err","P-value")
+	rownames(df.beta) <- paste0("b",0:(nrow(df.beta)-1) )
+
+	colnames(df.theta) <- c("Estimate","Std Err")
+	rownames(df.theta) <- c("Nugget","Partial Sill","Range")
 
 	ss <- list(
 		df.beta=df.beta,
@@ -16,7 +27,9 @@
 
 # print the summary
 "print.summary.spacious" <- function(x, ...) {
-	cat("Spacious fit:\n")
+	cat("Coefficients:\n")
 	print(x$df.beta)
+
+	cat("Spatial parameters\n")
 	print(x$df.theta)
 }
