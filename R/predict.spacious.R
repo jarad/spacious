4e84cc55571bd8c61007e886fc619c720dd0d674
Ryan J. Parker <ryan@basketballgeek.com>
2012-12-17 21:00:04 -0500

    Added support for newdata as a matrix, along with placing points into blocks based on centroids of blocks in the grid

--- R/predict.spacious.R
+++ R/predict.spacious.R
@@ -1,5 +1,5 @@
 # predict values at new sites using fit from spacious block composite model
-"predict.spacious" <- function(object, newdata=NULL, newS=NULL, D=NULL,
+"predict.spacious" <- function(object, newdata=NULL, newS=NULL, newB=NULL, D=NULL,
 	interval = "none", level = 0.95, ...) {
 
 # TODO: error check inputs
@@ -8,17 +8,30 @@
 	newS <- matrix(newS, ncol=2)
 
 	# number of blocks
-	nB <- length(object$grid)
+	nB   <- length(object$grid)
 	nFit <- nrow(object$S)
 	nNew <- nrow(newS)
 
 	# build model matrix for the new data
 	if (is.null(newdata)) {
-		X <- rep(1, nNew)
+		if (length(object$beta) > 1) {
+			# newdata missing, yet we have covariates...
+			stop("newdata must be specified for this fit.")
+		}
+
+		# use intercept only
+		X <- matrix(1, nrow=nNew, ncol=1)
 	} else {
-		terms <- delete.response(terms(object))
-		mf <- model.frame(terms, newdata)
-		X <- model.matrix(terms, mf)
+		if (is.data.frame(newdata)) {
+			terms <- delete.response(terms(object))
+			mf <- model.frame(terms, newdata)
+			X <- model.matrix(terms, mf)
+		} else if (is.matrix(newdata)) {
+			# assume newdata is a matrix of covariates
+			X <- cbind(1, newdata)
+		} else {
+			stop(paste0("Unable to handle newdata with class ", class(newdata)))
+		}
 	}
 
 	# locations for all points
@@ -39,7 +52,7 @@
 	}
 
 	# figure out newB the location is in
-	if (nB == 0) {  # no blocks
+	if (nB == 0 && is.null(newB)) {  # no blocks
 		# perform traditional kriging
 		n <- nrow(S)
 
@@ -58,14 +71,25 @@
 	} else {
 		# predict when we have blocks
 
-		# figure out newBs contain these new locations
-		newB <- rep(NA, nNew)
-
-		if (is.null(grid)) {
-			# we don't have polygons, so find the closest point and use that block
-			stop("No polygons defining blocks. Using the closest point to find a block.")
-# TODO: do this
+		if (!is.null(newB)) {
+			# make sure we have these blocks defined in the nieghbors
+			uniqueBlocks <- unique( as.vector( object$neighbors ) )
+			sapply(newB, function(b) {
+				if (sum(uniqueBlocks == b) == 0) {
+					stop(paste0("No neighbors defined for block ",b,". Unable to make predictions."))
+				}
+			})
 		} else {
+			# user did not define blocks, so find some
+
+			if (is.null(grid)) {
+				# we don't have polygons, so find the closest point and use that block
+				stop("No polygons defining blocks. Specify newB to make predictions.")
+			}
+
+			# figure out newBs contain these new locations
+			newB <- rep(NA, nNew)
+
 			for (b in 1:nB) {
 				in_poly <- point.in.polygon(newS[,1], newS[,2],
 					object$grid@polygons[[b]]@Polygons[[1]]@coords[,1],
@@ -74,47 +98,119 @@
 				newB[in_poly] <- b
 			}
 
-			if (sum(is.na(newB))) {
-				stop("Unable to find blocks for all prediction sites. Using closest point(s) to find block(s).")
-# TODO: do this
+			if (sum(is.na(newB)) > 0) {
+				warning("Unable to find blocks for all prediction locations. Assigning locations to blocks using block centroids.")
+
+				which.naB <- which( is.na(newB) )   # which obs are missing blocks?
+				N.naB     <- length(which.naB)      # how many of these are there?
+
+				# get centers of blocks
+				block.centers <- coordinates(object$grid)
+
+				# compute distances between block centers and obs with missing blocks
+				D.centers <- matrix( (rdist(rbind(newS[which.naB,], block.centers)))[1:N.naB, N.naB + 1:nrow(block.centers)], nrow=N.naB )
+
+				# find block for each missing
+				foundB <- apply(D.centers, 1, function(row) {
+					which.min(row)
+				})
+
+				# updated new blocks
+				newB[which.naB] <- foundB
 			}
+		}
 
-			# block memberships for all points
-			B <- c(object$B, newB)
+		# block memberships for all points
+		B <- c(object$B, newB)
 
-			# predict for each unique block
-			y_0[1:nNew] <- X %*% object$beta
+		# predict for each unique block
+		y_0[1:nNew] <- X %*% object$beta
 
-			# unique blocks where we have points to predict
-			uB <- unique(newB)
+		# unique blocks where we have points to predict
+		uB <- unique(newB)
 
 # TODO: we could re-construct this so that we only invert each matrix once. worth considering if speed more important than memory.
-			for (b in uB) {
-				# neighbors of this block
-				neighbors <- as.vector(object$neighbors[which( rowSums( object$neighbors==b ) == 1 ),])
-				neighbors <- neighbors[neighbors != b]
+		for (b in uB) {
+			# neighbors of this block
+			neighbors <- as.vector(object$neighbors[which( rowSums( object$neighbors==b ) == 1 ),])
+			neighbors <- neighbors[neighbors != b]
 
-				# what new points are in this block?
-				in.new   <- which(newB == b)+nFit
-				n.in.new <- length(in.new)
+			# what new points are in this block?
+			in.new   <- which(newB == b)+nFit
+			n.in.new <- length(in.new)
 
-				# what observed points are in this block?
-				in.obs   <- which(object$B == b)
-				n.in.obs <- length(in.obs)
+			# what observed points are in this block?
+			in.obs   <- which(object$B == b)
+			n.in.obs <- length(in.obs)
 
-				# new and observed in this block
-				in.b   <- c(in.new, in.obs)
-				n.in.b <- n.in.new + n.in.obs
+			# new and observed in this block
+			in.b   <- c(in.new, in.obs)
+			n.in.b <- n.in.new + n.in.obs
 
-				A_0 <- matrix(0, nrow=n.in.new, ncol=n.in.new)
-				b_0 <- rep(0, n.in.new)
+			A_0 <- matrix(0, nrow=n.in.new, ncol=n.in.new)
+			b_0 <- rep(0, n.in.new)
 
-				if (interval == "prediction") {
-					J_0 <- matrix(0, nrow=n.in.new, ncol=n.in.new)
-					B_0 <- matrix(0, nrow=n.in.new, ncol=n.in.new+n.in.obs)
-				}
+			if (interval == "prediction") {
+				J_0 <- matrix(0, nrow=n.in.new, ncol=n.in.new)
+				B_0 <- matrix(0, nrow=n.in.new, ncol=n.in.new+n.in.obs)
+			}
 
 # TODO: identify overlap of n1 and n2 so that some of these matrix computations might be reduced
+			for (n1 in neighbors) {
+				in.n1   <- which(object$B == n1)
+				n.in.n1 <- length(in.n1)
+
+				# take points so that we have Y = (Y_{b,new}, Y_{b,obs}, Y_{n1})
+				in.pair.n1 <- c(in.b, in.n1)
+
+				# covariance for b and n1
+				Sigma.n1    <- compute_cov(object$cov, object$theta, D[in.pair.n1,in.pair.n1])
+				invSigma.n1 <- chol2inv(chol(Sigma.n1))
+
+				A_0 <- A_0 + invSigma.n1[1:n.in.new,1:n.in.new]
+				b_0 <- b_0 + invSigma.n1[1:n.in.new,n.in.new+1:n.in.obs] %*% object$resids[in.obs] +
+					invSigma.n1[1:n.in.new,n.in.new+n.in.obs+1:n.in.n1] %*% object$resids[in.n1]
+
+				if (interval == "prediction") {
+					B_0 <- B_0 + cbind(
+						matrix(invSigma.n1[1:n.in.new,1:n.in.new],nrow=n.in.new,ncol=n.in.new),
+						matrix(invSigma.n1[1:n.in.new,n.in.new+1:n.in.obs],nrow=n.in.new,ncol=n.in.obs)
+					)
+
+					for (n2 in neighbors) {
+						in.n2   <- which(object$B == n2)
+						n.in.n2 <- length(in.n2)
+
+						# take points so that we have Y = (Y_{b,new}, Y_{b,obs}, Y_{n2})
+						in.pair.n2 <- c(in.b, in.n2)
+
+						# points for Y = (Y_{n1}, Y_{n2})
+						in.n1n2 <- c(in.n1, in.n2)
+
+						# covariance for b and n2
+						Sigma.n2    <- compute_cov(object$cov, object$theta, D[in.pair.n2,in.pair.n2])
+						invSigma.n2 <- chol2inv(chol(Sigma.n2))
+
+						# covariance for n1 and n2
+						Sigma.n1n2 <- compute_cov(object$cov, object$theta, D[in.n1n2,in.n1n2])
+
+						J_0 <- J_0 + invSigma.n1[1:n.in.new,n.in.new+n.in.obs+1:n.in.n1] %*%
+							Sigma.n1n2[1:n.in.n1,n.in.n1+1:n.in.n2] %*% invSigma.n2[n.in.new+n.in.obs+1:n.in.n2,1:n.in.new]
+					}
+				}
+			}
+
+			# complete predictions
+			b_0 <- -b_0
+			y_0[in.new-nFit] <- y_0[in.new-nFit] + chol2inv(chol(A_0)) %*% b_0
+
+			if (interval == "prediction") {
+				# complete prediction variances
+				H_0 <- -A_0
+
+				Sigma <- compute_cov(object$cov, object$theta, D[in.b,in.b])
+				J_0   <- J_0 + B_0 %*% Sigma %*% t(B_0)
+
 				for (n1 in neighbors) {
 					in.n1   <- which(object$B == n1)
 					n.in.n1 <- length(in.n1)
@@ -126,68 +222,12 @@
 					Sigma.n1    <- compute_cov(object$cov, object$theta, D[in.pair.n1,in.pair.n1])
 					invSigma.n1 <- chol2inv(chol(Sigma.n1))
 
-					A_0 <- A_0 + invSigma.n1[1:n.in.new,1:n.in.new]
-					b_0 <- b_0 + invSigma.n1[1:n.in.new,n.in.new+1:n.in.obs] %*% object$resids[in.obs] +
-						invSigma.n1[1:n.in.new,n.in.new+n.in.obs+1:n.in.n1] %*% object$resids[in.n1]
-
-					if (interval == "prediction") {
-						B_0 <- B_0 + cbind(
-							matrix(invSigma.n1[1:n.in.new,1:n.in.new],nrow=n.in.new,ncol=n.in.new),
-							matrix(invSigma.n1[1:n.in.new,n.in.new+1:n.in.obs],nrow=n.in.new,ncol=n.in.obs)
-						)
-
-						for (n2 in neighbors) {
-							in.n2   <- which(object$B == n2)
-							n.in.n2 <- length(in.n2)
-
-							# take points so that we have Y = (Y_{b,new}, Y_{b,obs}, Y_{n2})
-							in.pair.n2 <- c(in.b, in.n2)
-
-							# points for Y = (Y_{n1}, Y_{n2})
-							in.n1n2 <- c(in.n1, in.n2)
-
-							# covariance for b and n2
-							Sigma.n2    <- compute_cov(object$cov, object$theta, D[in.pair.n2,in.pair.n2])
-							invSigma.n2 <- chol2inv(chol(Sigma.n2))
-
-							# covariance for n1 and n2
-							Sigma.n1n2 <- compute_cov(object$cov, object$theta, D[in.n1n2,in.n1n2])
-
-							J_0 <- J_0 + invSigma.n1[1:n.in.new,n.in.new+n.in.obs+1:n.in.n1] %*%
-								Sigma.n1n2[1:n.in.n1,n.in.n1+1:n.in.n2] %*% invSigma.n2[n.in.new+n.in.obs+1:n.in.n2,1:n.in.new]
-						}
-					}
+					J_0 <- J_0 + 2 * B_0 %*% Sigma.n1[1:n.in.b,n.in.b+1:n.in.n1] %*% invSigma.n1[n.in.b+1:n.in.n1,1:n.in.new]
 				}
-
-				# complete predictions
-				b_0 <- -b_0
-				y_0[in.new-nFit] <- y_0[in.new-nFit] + chol2inv(chol(A_0)) %*% b_0
-
-				if (interval == "prediction") {
-					# complete prediction variances
-					H_0 <- -A_0
-
-					Sigma <- compute_cov(object$cov, object$theta, D[in.b,in.b])
-					J_0   <- J_0 + B_0 %*% Sigma %*% t(B_0)
-
-					for (n1 in neighbors) {
-						in.n1   <- which(object$B == n1)
-						n.in.n1 <- length(in.n1)
-
-						# take points so that we have Y = (Y_{b,new}, Y_{b,obs}, Y_{n1})
-						in.pair.n1 <- c(in.b, in.n1)
-
-						# covariance for b and n1
-						Sigma.n1    <- compute_cov(object$cov, object$theta, D[in.pair.n1,in.pair.n1])
-						invSigma.n1 <- chol2inv(chol(Sigma.n1))
-
-						J_0 <- J_0 + 2 * B_0 %*% Sigma.n1[1:n.in.b,n.in.b+1:n.in.n1] %*% invSigma.n1[n.in.b+1:n.in.n1,1:n.in.new]
-					}
-					sd.pred[in.new-nFit] <- sqrt(diag( chol2inv(chol(H_0 %*% chol2inv(chol(J_0)) %*% H_0)) ))
-				}
+				sd.pred[in.new-nFit] <- sqrt(diag( chol2inv(chol(H_0 %*% chol2inv(chol(J_0)) %*% H_0)) ))
 			}
 		}
-	}
+	} # end prediction
 
 	if (interval == "prediction") {
 		tile <- qnorm((1-level)/2, lower.tail=FALSE)
