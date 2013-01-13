6ea7aa2bd67852667c0f66cdb9688b21092b5e17
Ryan J. Parker <ryan@basketballgeek.com>
2013-01-13 18:20:12 -0500

    Use x instead of object for S3 method plot

--- R/plot.spacious.R
+++ R/plot.spacious.R
@@ -1,11 +1,11 @@
 # plot dianostics of model fit
-"plot.spacious" <- function(object) {
+"plot.spacious" <- function(x) {
 
 	# plot spatial parameter estimates vs iteration
 	par(mfrow=c(2,2))
-	plot(0:object$nIter, object$iters.theta[,1], type="b", xlab="Iteration", ylab="Nugget")
-	plot(0:object$nIter, object$iters.theta[,2], type="b", xlab="Iteration", ylab="Partial Sill")
-	plot(0:object$nIter, object$iters.theta[,3], type="b", xlab="Iteration", ylab="Range")
-	plot(0:object$nIter, object$iters.ll, type="b", xlab="Iteration", ylab="-2 * log likelihood")
+	plot(0:x$nIter, x$iters.theta[,1], type="b", xlab="Iteration", ylab="Nugget")
+	plot(0:x$nIter, x$iters.theta[,2], type="b", xlab="Iteration", ylab="Partial Sill")
+	plot(0:x$nIter, x$iters.theta[,3], type="b", xlab="Iteration", ylab="Range")
+	plot(0:x$nIter, x$iters.ll, type="b", xlab="Iteration", ylab="-2 * log likelihood")
 
 }
