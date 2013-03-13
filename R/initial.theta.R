7d7966870a50743484d7befcfc868da5148af552
Ryan J. Parker <ryan@basketballgeek.com>
2013-03-13 19:27:42 -0400

    Constrained initial nugget to be at least 0.5

--- R/initial.theta.R
+++ R/initial.theta.R
@@ -1,16 +1,19 @@
 # Computes initial values for matern covariance matrix
-initial.theta = function(y, S, p.nugget=0.1, p.range=0.1) {
-  sill = var(y)              # 
-  nugget = p.nugget*sill
-  partial.sill = sill-nugget
+"initial.theta" <- function(y, S, p.nugget=0.1, p.range=0.1) {
+	sill         <- var(y)
+	nugget       <- p.nugget*sill
+	partial.sill <- sill-nugget
+
+	# make sure initial nugget isn't too small
+	nugget <- max(nugget, 0.5)
 
   # Calculate <p.range>th percentile of distance between 1000 random points
-  n = length(y)
-  points = sample(n, min(n,1000))
-  d = dist(S[points,])
-  range = quantile(d,p.range)
+  n      <- length(y)
+  points <- sample(n, min(n,1000))
+  d      <- dist(S[points,])
+  range  <- quantile(d,p.range)
   
-  smoothness = 0.5
+  smoothness <- 0.5
 
   return(c(nugget, partial.sill, 1/range, smoothness))
 }
