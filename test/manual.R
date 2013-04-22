9bca747b0c17cf59c7f559018bc7db97ccc20fec
Ryan J. Parker <ryan@basketballgeek.com>
2013-04-22 11:07:21 -0400

    Added prediction SD to manual

--- test/manual.R
+++ test/manual.R
@@ -4,17 +4,21 @@ options(digits=2)
 if ("package:spacious" %in% search()) {
   # unload the package
   detach("package:spacious", unload=TRUE)
+	library.dynam.unload("spacious", libpath="~/Rlib/spacious")
 }
 
 # load the package
 library(spacious)
 data(anom.2011)
 
+library(ggmap)
+euro <- get_map(location=c(mean(anom.2011$lon),mean(anom.2011$lat)), zoom=4, maptype="satellite")
+
+S <- cbind(anom.2011$lon, anom.2011$lat)
+
 if (FALSE) {
 # plot data
-library(ggmap)
 
-euro <- get_map(location=c(mean(anom.2011$lon),mean(anom.2011$lat)), zoom=4, maptype="satellite")
 fig  <- ggmap(euro) +
         geom_point(aes(lon, lat, color=anom), shape=15, size=1.5, data=anom.2011) +
         scale_colour_gradient(low="green", high="red")
@@ -25,7 +29,6 @@ graphics.off()
 
 if (FALSE) {
 # basic call
-S   <- cbind(anom.2011$lon, anom.2011$lat)
 fit <- spacious(anom ~ lon + lat + elev, S=S, data=anom.2011)
 pdf("pdf/manual_fit.pdf")
 	plot(fit)
@@ -48,32 +51,41 @@ print(summary(fit))
 if (FALSE) {
 # blocking structures
 library(maps)
-fit_c <- spacious(anom ~ lon + lat + elev, S=S, data=anom.2011, blocks=list(type="cluster"))
-fit_r <- spacious(anom ~ lon + lat + elev, S=S, data=anom.2011, blocks=list(type="regular"))
+#fit_c <- spacious(anom ~ lon + lat + elev, S=S, data=anom.2011, blocks=list(type="cluster"))
+#fit_r <- spacious(anom ~ lon + lat + elev, S=S, data=anom.2011, blocks=list(type="regular"))
 
 pdf("pdf/manual_blocks_c.pdf")
-	plot(S, col=fit_c$B, pch=15, xlab="lon", ylab="lat", main="Cluster blocks")
-	map("world", add=TRUE)
-	plot(fit_c$grid,lty=1,border="gray",cex=0.25,add=TRUE)
+	plot(S, pch=4, xlab="lon", ylab="lat", main="Cluster blocks", cex=0.5)
+	map("world", add=TRUE, col="darkgreen")
+	plot(fit_c$grid,lty=1,lwd=1.5,border="gray",cex=0.25,add=TRUE)
 graphics.off()
 
 pdf("pdf/manual_blocks_r.pdf")
-	plot(S, col=fit_r$B, pch=15, xlab="lon", ylab="lat", main="Regular blocks")
-	map("world", add=TRUE)
-	plot(fit_r$grid,lty=1,border="gray",cex=0.25,add=TRUE)
+	plot(S, pch=4, xlab="lon", ylab="lat", main="Regular blocks", cex=0.5)
+	map("world", add=TRUE, col="darkgreen")
+	plot(fit_r$grid,lty=1,lwd=1.5,border="gray",cex=0.25,add=TRUE)
 graphics.off()
 }
 
-if (FALSE) {
+if (TRUE) {
 # predictions
-fit <- spacious(anom~lon + lat + elev, S=S, data=anom.2011)
-S.p <- cbind(anom.pred.grid$lon, anom.pred.grid$lat)
-pred <- predict(fit, newdata=anom.pred.grid, newS=S.p)$y
+#fit     <- spacious(anom~lon + lat + elev, S=S, data=anom.2011)
+S.p     <- cbind(anom.pred.grid$lon, anom.pred.grid$lat)
+preds   <- predict(fit, newdata=anom.pred.grid, newS=S.p, interval="prediction")
+pred    <- preds$y
+pred.sd <- preds$sd
 
-fig <- ggmap(euro) +
+fig <- ggmap(euro) + ggtitle("Predictions") +
        geom_point(aes(lon,lat,color=pred), shape=15, size=1.25, data=data.frame(anom.pred.grid, pred=pred))+
        scale_colour_gradient(low="green", high="red")
 pdf("pdf/manual_pred.pdf")
+	print(fig)
+graphics.off()
+
+fig <- ggmap(euro) + ggtitle("Prediction SD") +
+       geom_point(aes(lon,lat,color=pred.sd), shape=15, size=1.25, data=data.frame(anom.pred.grid, pred.sd=pred.sd))+
+       scale_colour_gradient(low="green", high="red")
+pdf("pdf/manual_pred_sd.pdf")
 	print(fig)
 graphics.off()
 }
